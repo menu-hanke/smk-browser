3b281bef7064a96cde5dd73dc6c21f2f43f59509
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-09-27 14:40:32 +0300

    Created theme for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import './App.css';
 import MainView from './views/mainView';
 import { SnackbarProvider } from 'notistack';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles'
+import { green } from '@material-ui/core/colors'
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: green[700]
+      }
+    }
+  })
+
+
   return (
     <div>
-      <SnackbarProvider maxSnack={5}>
-        <MainView />
-      </SnackbarProvider>
+      <MuiThemeProvider theme={theme}>
+        <SnackbarProvider maxSnack={5}>
+          <MainView />
+        </SnackbarProvider>
+      </MuiThemeProvider>
     </div>
   );
 }
