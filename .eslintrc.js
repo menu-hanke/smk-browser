8ade6daf9de6ee3bbde89282e5f21fe8479071be
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-05 14:57:37 +0200

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,34 @@
 module.exports = {
   extends: 'erb',
   rules: {
+    'prettier/prettier': 0,
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
     // Since React 17 and typescript 4.1 you can safely disable the rule
     'react/react-in-jsx-scope': 'off',
+    'no-console': 'warn',
+    'semi': ['warn', 'never'],
+    'comma-dangle': ['warn', 'never'],
+    "@typescript-eslint/ban-ts-comment": "off",
+    '@typescript-eslint/no-use-before-define': 'off'
   },
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
-    createDefaultProgram: true,
+    createDefaultProgram: true
   },
   settings: {
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
       webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
-      },
+        config: require.resolve('./.erb/configs/webpack.config.eslint.ts')
+      }
     },
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-  },
-};
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    }
+  }
+}
