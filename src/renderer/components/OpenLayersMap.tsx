93bf1d83e8ffe2e0cbdd38a05bfdc498c281d64d
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-10 14:30:52 +0200

    added background map to OL

--- src/renderer/components/OpenLayersMap.tsx
+++ src/renderer/components/OpenLayersMap.tsx
@@ -1,53 +1,62 @@
+/* eslint-disable prefer-const */
+/* eslint-disable prefer-template */
+/* eslint-disable object-shorthand */
 import * as React from 'react'
 import {createStyles, makeStyles} from '@material-ui/styles'
 import Projection from 'ol/proj/Projection'
 import * as ol from 'ol'
 import 'ol/ol.css'
-import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS';
 import TileLayer from 'ol/layer/Tile';
-import WMTSCapabilities from 'ol/format/WMTSCapabilities';
+import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS'
+import WMTSCapabilities from 'ol/format/WMTSCapabilities'
+import proj4 from 'proj4'
+// import {ScaleLine, defaults as defaultControls} from 'ol/control';
+// import {fromLonLat} from 'ol/proj';
+import {register} from 'ol/proj/proj4';
 import {apiKey} from '../../../apiKey.json'
 
 const projection = new Projection({
-  code: 'EPSG:3067', 
-  extent:  [50199.4814, 6582464.0358, 761274.6247, 7799839.8902]
+  code: 'EPSG:3067',
+  extent: [50199.4814, 6582464.0358, 761274.6247, 7799839.8902]
 })
+
+proj4.defs("EPSG:3067","+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+register(proj4)
 
 const OpenLayersMap: React.FC = () => {
   const [map, setMap] = React.useState<any>()
   const mapRef = React.useRef<HTMLElement>()
-
   const mapExtent = {
     center: [397915, 7132330],
     resolution: 3550,
     rotation: 0,
-    projection
-}
+    projection: projection
+  }
 
 console.log(map)
 
-
-
 const initializeOL = React.useCallback(async() => {
-
-  // Fetch map
+  const parser = new WMTSCapabilities()
   const url = 'https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/WMTSCapabilities.xml?api-key='
-  
-  const parser = new WMTSCapabilities();
   const response = await fetch(url + apiKey)
   console.log('response from fetch: ', response)
-  const text= await response.text()
+  const text = await response.text()
   console.log('text from response: ', text)
   const result = await parser.read(text)
   console.log('result from parser: ', result)
 
-  const options = optionsFromCapabilities(result, {
+  let options = optionsFromCapabilities(result, {
     layer: 'maastokartta', // tai 'ortokuva'
-    matrixSet: 'ETRS-TM35FIN',
-  });
+    projection: projection
+  })
 
+  if (options && options.urls) {
+    const urlToEdit = options.urls[0]
+    options.urls[0] = `${urlToEdit}api-key=${apiKey}`
+    console.log('options after edit: ', options)
+  }
 
-
+  console.log('options from catabilities: ', options)
   console.log('Initializing Open Layers')
   const newMap = new ol.Map({
     target: mapRef.current, 
@@ -65,6 +74,7 @@ const initializeOL = React.useCallback(async() => {
     })
   })
   return newMap
+
 }, [mapRef])
 
 // OnInit
