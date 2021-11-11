cb7aa565dd8615043be862bff60230268c1858ff
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-11 17:25:05 +0200

    Updated Modal component to contain Map and MapPatchBrowser components

--- src/renderer/components/ModalComponent.tsx
+++ src/renderer/components/ModalComponent.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { createStyles, makeStyles } from '@mui/styles'
 import { Box, Grid, Modal } from '@material-ui/core'
 import OpenLayersMap from '../components/OpenLayersMap'
+import MapPatchBrowser from './MapPatchBrowser'
 
 interface ModalProps {
  modalIsOpen: boolean
@@ -11,14 +12,19 @@ interface ModalProps {
 const ModalComponent: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
  const classes = useStyles()
  return (
-  <Modal open={modalIsOpen} onClose={closeModal} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+  <Modal
+   open={modalIsOpen}
+   onClose={closeModal}
+   aria-labelledby="modal-modal-title"
+   aria-describedby="modal-modal-description"
+  >
    <Box className={classes.modalContainer}>
     <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
      <Grid container item xs={10} justifyContent="center" alignItems="center">
       <OpenLayersMap />
      </Grid>
      <Grid container item xs={2} justifyContent="center">
-      <button>this is a button</button>
+      <MapPatchBrowser />
      </Grid>
     </Grid>
    </Box>
@@ -30,11 +36,11 @@ const useStyles = makeStyles(() =>
  createStyles({
   modalContainer: {
    position: 'absolute',
-   top: '55%',
+   top: '53%',
    left: '50%',
    transform: 'translate(-50%, -50%)',
    width: '98%',
-   height: '80%',
+   height: '75%',
    backgroundColor: 'white',
    borderRadius: '5px',
    boxShadow: '24',
