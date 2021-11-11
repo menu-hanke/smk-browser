fba6a15ac0063a9d39444ade47407898a46948a9
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-11 11:08:52 +0200

    create MapPatchBrowser component

---
+++ src/renderer/components/MapPatchBrowser.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import {Box, Grid, }
+
+// interface interfaceName {
+//   value: string
+// }
+
+const MapPatchBrowser: React.FC = () => {
+const classes = useStyles()
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+const useStyles = makeStyles(() =>
+  createStyles({
+
+  }))
+
+export default MapPatchBrowser
