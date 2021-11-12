500ce14275da4fdbfbc9055e48ef29453ab6a598
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-12 14:59:53 +0200

    added redux store

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
 import { SnackbarProvider } from 'notistack'
+import { createStore, combineReducers, compose } from 'redux'
+
+import { Provider } from 'react-redux'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { createTheme } from '@material-ui/core'
 import { green } from '@material-ui/core/colors'
 import MainView from './views/mainView'
+import dataReducer from './Store/Reducers/data'
 
-export default function App() {
+declare global {
+ interface Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+ }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const rootReducer = combineReducers(dataReducer)
+const store = createStore(rootReducer, composeEnhancers())
+
+const App = () => {
  const theme = createTheme({
   palette: {
    primary: {
@@ -18,12 +32,16 @@ export default function App() {
  })
 
  return (
-  <div>
+  <Provider store={store}>
    <ThemeProvider theme={theme}>
     <SnackbarProvider maxSnack={1}>
      <MainView />
     </SnackbarProvider>
    </ThemeProvider>
-  </div>
+  </Provider>
  )
 }
+
+export default App
+export { store }
+export type RootState = ReturnType<typeof store.getState>
