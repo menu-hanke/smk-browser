0c8a933c4567730a237fc93602fdd016d15ca11e
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-17 15:39:23 +0200

    created actions for state changes

--- src/renderer/Store/Actions/data.ts
+++ src/renderer/Store/Actions/data.ts
@@ -1,18 +1,43 @@
 export const ADD_DATA_TO_STORE = 'ADD_DATA_TO_STORE'
-export const SAVE_FOUND_ID = 'SAVE_FOUND_ID'
-export const SAVE_FOUND_STAND_IDS = 'SAVE_FOUND_STAND_IDS'
+export const SET_FOUND_ID = 'SET_FOUND_ID'
+export const SET_FOUND_STAND_IDS = 'SET_FOUND_STAND_IDS'
+export const SET_PROPERTY_IDS = 'SET_PROPERTY_IDS'
+export const SET_FOREST_STAND_VERSION = 'SET_FOREST_STAND_VERSION'
+export const SET_FOLDER_PATH = 'SET_FOLDER_PATH'
+export const SET_LOG_DATA = 'SET_LOG_DATA'
+export const SET_MODAL_STATE = 'SET_MODAL_STATE'
 
-export const addDataToStore = (data: any) => ({
- type: ADD_DATA_TO_STORE,
+export const setFoundId = (data: any) => ({
+ type: SET_FOUND_ID,
  payload: data
 })
 
-export const saveFoundId = (data: any) => ({
- type: SAVE_FOUND_ID,
+export const setFoundStandIds = (data: any) => ({
+ type: SET_FOUND_STAND_IDS,
  payload: data
 })
 
-export const saveFoundStandIds = (data: any) => ({
- type: SAVE_FOUND_STAND_IDS,
+export const setPropertyIds = (data: any) => ({
+ type: SET_PROPERTY_IDS,
+ payload: data
+})
+
+export const setForestStandVersion = (data: any) => ({
+ type: SET_FOREST_STAND_VERSION,
+ payload: data
+})
+
+export const setFolderPath = (data: any) => ({
+ type: SET_FOLDER_PATH,
+ payload: data
+})
+
+export const setLogData = (data: any) => ({
+ type: SET_LOG_DATA,
+ payload: data
+})
+
+export const setModalState = (data: any) => ({
+ type: SET_MODAL_STATE,
  payload: data
 })
