e3adfda6bf986db147048e8a6d902898e80d45a0
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-17 12:36:31 +0200

    removed DropdownSelext from MapPatchBrowser

--- src/renderer/components/MapPatchBrowser.tsx
+++ src/renderer/components/MapPatchBrowser.tsx
@@ -3,7 +3,6 @@ import { createStyles, makeStyles } from '@mui/styles'
 import { Grid } from '@material-ui/core'
 import IconButton from '@mui/material/IconButton'
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material'
-import DropdownSelect from './DropdownSelect'
 
 // interface interfaceName {
 //   value: string
@@ -12,15 +11,6 @@ import DropdownSelect from './DropdownSelect'
 const MapPatchBrowser: React.FC = () => {
  const classes = useStyles()
 
- const arrayOfItems = [
-  'PropertyID:  213-424-1-120 patch: 1',
-  'PropertyID:  213-424-1-120 patch: 2',
-  'PropertyID:  213-424-1-120 patch: 3',
-  'PropertyID:  213-424-1-120 patch: 4',
-  'PropertyID:  213-424-1-120 patch: 5',
-  'PropertyID:  213-424-1-120 patch: 6'
- ]
-
  return (
   <Grid container className={classes.container} alignItems="center" justifyContent="space-around">
    <Grid container item xs={2} justifyContent="center" alignItems="center">
@@ -28,13 +18,7 @@ const MapPatchBrowser: React.FC = () => {
      <KeyboardArrowLeft fontSize="large" />
     </IconButton>
    </Grid>
-   <Grid container item xs={8} justifyContent="center">
-    <DropdownSelect
-     arrayOfItems={arrayOfItems}
-     afterSelectFunction={() => console.log('function called')}
-     isLogData={false}
-    />
-   </Grid>
+   <Grid container item xs={8} justifyContent="center"></Grid>
    <Grid container item xs={2} justifyContent="center">
     <IconButton size="large">
      <KeyboardArrowRight fontSize="large" />
