4fad73b6f7ffa83db63e42d60084725a29820127
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-17 12:37:15 +0200

    created new actions to save found property Ids and found stand ids

--- src/renderer/Store/Actions/data.ts
+++ src/renderer/Store/Actions/data.ts
@@ -1,6 +1,18 @@
 export const ADD_DATA_TO_STORE = 'ADD_DATA_TO_STORE'
+export const SAVE_FOUND_ID = 'SAVE_FOUND_ID'
+export const SAVE_FOUND_STAND_IDS = 'SAVE_FOUND_STAND_IDS'
 
 export const addDataToStore = (data: any) => ({
  type: ADD_DATA_TO_STORE,
+ payload: data
+})
+
+export const saveFoundId = (data: any) => ({
+ type: SAVE_FOUND_ID,
+ payload: data
+})
+
+export const saveFoundStandIds = (data: any) => ({
+ type: SAVE_FOUND_STAND_IDS,
  payload: data
 })
