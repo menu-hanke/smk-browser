a00efcb02b602092308e7639d273fa01a2ede565
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-11-17 15:39:05 +0200

    refactored modal component to use redux

--- src/renderer/components/ModalComponent.tsx
+++ src/renderer/components/ModalComponent.tsx
@@ -3,21 +3,16 @@ import { createStyles, makeStyles } from '@mui/styles'
 import { Box, Grid, Modal } from '@material-ui/core'
 import OpenLayersMap from '../components/OpenLayersMap'
 import MapPatchBrowser from './MapPatchBrowser'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from 'renderer/App'
+import { setModalState } from 'renderer/Store/Actions/data'
 
-interface ModalProps {
- modalIsOpen: boolean
- closeModal: () => void
-}
-
-const ModalComponent: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
+const ModalComponent: React.FC = () => {
  const classes = useStyles()
+ const dispatch = useDispatch()
+ const displayMap = useSelector((state: RootState) => state.map.displayMap)
  return (
-  <Modal
-   open={modalIsOpen}
-   onClose={closeModal}
-   aria-labelledby="modal-modal-title"
-   aria-describedby="modal-modal-description"
-  >
+  <Modal open={displayMap} onClose={() => dispatch(setModalState({ displayMap: false }))} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
    <Box className={classes.modalContainer}>
     <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
      <Grid container item xs={10} justifyContent="center" alignItems="center">
