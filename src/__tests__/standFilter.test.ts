9de00f403cb0f70a6015e766cb85c7649e66271e
Sampo Savolainen <sampo.savolainen@spatineo.com>
2021-11-19 15:05:59 +0200

    More tests for geographic filtering

--- src/__tests__/standFilter.test.ts
+++ src/__tests__/standFilter.test.ts
@@ -7,7 +7,13 @@ import { filterStands } from '../renderer/controllers/standFilter'
 
 const data1Geometry = wkt.parse("POLYGON((393960.156 6801453.126, 394798.608 6801657.878, 394930.512 6801670.111, 395028.723 6802116.858, 394258.945 6801929.148, 394261.711 6801810.541, 394091.166 6801665.961, 393960.156 6801453.126))")
 
-describe('App', () => {
+const bboxToPolygon = ([x1,y1,x2,y2] : number[]) => wkt.parse(`POLYGON((${x1} ${y1}, ${x2} ${y1}, ${x2} ${y2}, ${x1} ${y2}, ${x1} ${y1}))`);
+const finlandGeometry = bboxToPolygon([-3669433.90, 4601644.86, 648181.26, 9364104.12])
+
+const southernFinlandGeometry = bboxToPolygon([-3669433.90, 4601644.86, 648181.26, 6982874.49])
+const northernFinlandGeometry = bboxToPolygon([-3669433.90, 6982874.49, 648181.26, 9364104.12])
+
+describe('standFilter', () => {
   it('filtering should maybe filter 50 stands to 29', async () => {
     const xmlString = await readFile('src/__tests__/data1-stands.test.xml')
     let xml = await xml2js.parseStringPromise(xmlString)
@@ -17,5 +23,38 @@ describe('App', () => {
     const result = filterStands(xml, data1Geometry, [], true);
 
     expect(result.arrayOfStandIds.length).toBe(29);
+  });
+
+  it('all 50 stands should be in Finland', async () => {
+    const xmlString = await readFile('src/__tests__/data1-stands.test.xml')
+    let xml = await xml2js.parseStringPromise(xmlString)
+
+    expect(xml['ForestPropertyData']['st:Stands'][0]['st:Stand'].length).toBe(50);
+
+    const result = filterStands(xml, finlandGeometry, [], true);
+
+    expect(result.arrayOfStandIds.length).toBe(50);
+  });
+
+  it('all 50 stands should be in Southern Finland', async () => {
+    const xmlString = await readFile('src/__tests__/data1-stands.test.xml')
+    let xml = await xml2js.parseStringPromise(xmlString)
+
+    expect(xml['ForestPropertyData']['st:Stands'][0]['st:Stand'].length).toBe(50);
+
+    const result = filterStands(xml, southernFinlandGeometry, [], true);
+
+    expect(result.arrayOfStandIds.length).toBe(50);
+  });
+
+  it('no stands should be in Northern Finland', async () => {
+    const xmlString = await readFile('src/__tests__/data1-stands.test.xml')
+    let xml = await xml2js.parseStringPromise(xmlString)
+
+    expect(xml['ForestPropertyData']['st:Stands'][0]['st:Stand'].length).toBe(50);
+
+    const result = filterStands(xml, northernFinlandGeometry, [], true);
+
+    expect(result.arrayOfStandIds.length).toBe(0);
   });
 });
