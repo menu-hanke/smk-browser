43c5b9f26c86a8dd783f4ef99faecf18cde05398
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-12-15 15:13:03 +0200

    added disabled functionality to dropdown select

--- src/renderer/components/DropdownSelect.tsx
+++ src/renderer/components/DropdownSelect.tsx
@@ -8,38 +8,43 @@ import { setDisplayMap, setSelectedPropertyIdForMap } from '../Store/Actions/dat
 const ITEM_HEIGHT = 48
 const ITEM_PADDING_TOP = 8
 const MenuProps = {
- PaperProps: {
-  style: {
-   maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-   width: 250
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250
+    }
   }
- }
 }
 
-const DropdownSelect: React.FC = () => {
- const dispatch = useDispatch()
- const foundIDs = useSelector((state: RootState) => state.saveProcess.foundIDs)
- const selectedPropertyId = useSelector((state: RootState) => state.map.selectedPropertyId)
+interface Props {
+  isDisabled: boolean
+}
 
- return (
-  <FormControl fullWidth>
-   <InputLabel id="demo-simple-select-label">Select ID to display</InputLabel>
-   <Select
-    fullWidth
-    label="select ID to display"
-    value={selectedPropertyId}
-    onChange={(event) => {
-     dispatch(setDisplayMap({ displayMap: true }))
-     dispatch(setSelectedPropertyIdForMap({ selectedPropertyId: event.target.value }))
-    }}
-    MenuProps={MenuProps}
-   >
-    {foundIDs.map((object: any) => (
-     <MenuItem key={object.propertyId} value={object.propertyId}>{`propertyID: ${object.propertyId}`}</MenuItem>
-    ))}
-   </Select>
-  </FormControl>
- )
+const DropdownSelect: React.FC<Props> = ({ isDisabled }: Props) => {
+  const dispatch = useDispatch()
+  const foundIDs = useSelector((state: RootState) => state.saveProcess.foundIDs)
+  const selectedPropertyId = useSelector((state: RootState) => state.map.selectedPropertyId)
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Select ID to display</InputLabel>
+      <Select
+        fullWidth
+        label="select ID to display"
+        value={selectedPropertyId}
+        disabled={isDisabled}
+        onChange={(event) => {
+          dispatch(setDisplayMap({ displayMap: true }))
+          dispatch(setSelectedPropertyIdForMap({ selectedPropertyId: event.target.value }))
+        }}
+        MenuProps={MenuProps}
+      >
+        {foundIDs.map((object: any) => (
+          <MenuItem key={object.propertyId} value={object.propertyId}>{`propertyID: ${object.propertyId}`}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  )
 }
 
 export default DropdownSelect
