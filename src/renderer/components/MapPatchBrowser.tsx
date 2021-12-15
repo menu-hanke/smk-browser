37eee54c762f2cdf359c8e5a6179a259cabb845b
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-12-15 15:13:29 +0200

    added disabled prop, formatting code

--- src/renderer/components/MapPatchBrowser.tsx
+++ src/renderer/components/MapPatchBrowser.tsx
@@ -9,83 +9,83 @@ import { RootState } from 'renderer/App'
 import { setDisplayMap, setSelectedPropertyIdForMap } from 'renderer/Store/Actions/data'
 
 const MapPatchBrowser: React.FC = () => {
- const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
- const foundIDs = useSelector((state: RootState) => state.saveProcess.foundIDs)
- const selectedPropertyId = useSelector((state: RootState) => state.map.selectedPropertyId)
- const currentIndex = foundIDs.findIndex((object) => object.propertyId === selectedPropertyId)
+  const foundIDs = useSelector((state: RootState) => state.saveProcess.foundIDs)
+  const selectedPropertyId = useSelector((state: RootState) => state.map.selectedPropertyId)
+  const currentIndex = foundIDs.findIndex((object) => object.propertyId === selectedPropertyId)
 
- const leftKeyPressed = () => {
-  const foundObject = foundIDs[currentIndex - 1]
-  if (foundObject) {
-   dispatch(setSelectedPropertyIdForMap({ selectedPropertyId: foundObject.propertyId }))
+  const leftKeyPressed = () => {
+    const foundObject = foundIDs[currentIndex - 1]
+    if (foundObject) {
+      dispatch(setSelectedPropertyIdForMap({ selectedPropertyId: foundObject.propertyId }))
+    }
   }
- }
 
- const rightKeyPressed = () => {
-  const foundObject = foundIDs[currentIndex + 1]
-  if (foundObject) {
-   dispatch(setSelectedPropertyIdForMap({ selectedPropertyId: foundObject.propertyId }))
+  const rightKeyPressed = () => {
+    const foundObject = foundIDs[currentIndex + 1]
+    if (foundObject) {
+      dispatch(setSelectedPropertyIdForMap({ selectedPropertyId: foundObject.propertyId }))
+    }
   }
- }
 
- const escKeyPressed = () => {
-  dispatch(setDisplayMap({ displayMap: false }))
- }
-
- const keyDownHandler = (event: any) => {
-  switch (event.keyCode) {
-   case 27:
-    escKeyPressed()
-    return
-
-   case 37:
-    leftKeyPressed()
-    return
-
-   case 39:
-    rightKeyPressed()
-    return
-
-   default:
-    return
+  const escKeyPressed = () => {
+    dispatch(setDisplayMap({ displayMap: false }))
   }
- }
 
- React.useEffect(() => {
-  window.addEventListener('keydown', keyDownHandler)
-  return () => {
-   window.removeEventListener('keydown', keyDownHandler)
+  const keyDownHandler = (event: any) => {
+    switch (event.keyCode) {
+      case 27:
+        escKeyPressed()
+        return
+
+      case 37:
+        leftKeyPressed()
+        return
+
+      case 39:
+        rightKeyPressed()
+        return
+
+      default:
+        return
+    }
   }
- })
 
- return (
-  <Grid container alignItems="center" justifyContent="space-around">
-   <Grid container item xs={3} justifyContent="center" alignItems="center">
-    <IconButton
-     size="large"
-     onClick={() => {
-      leftKeyPressed()
-     }}
-    >
-     <KeyboardArrowLeft fontSize="large" />
-    </IconButton>
-   </Grid>
-   <Grid container item xs={6} justifyContent="center">
-    <DropdownSelect />
-   </Grid>
-   <Grid container item xs={3} justifyContent="center">
-    <IconButton
-     size="large"
-     onClick={() => {
-      rightKeyPressed()
-     }}
-    >
-     <KeyboardArrowRight fontSize="large" />
-    </IconButton>
-   </Grid>
-  </Grid>
- )
+  React.useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler)
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler)
+    }
+  })
+
+  return (
+    <Grid container alignItems="center" justifyContent="space-around">
+      <Grid container item xs={3} justifyContent="center" alignItems="center">
+        <IconButton
+          size="large"
+          onClick={() => {
+            leftKeyPressed()
+          }}
+        >
+          <KeyboardArrowLeft fontSize="large" />
+        </IconButton>
+      </Grid>
+      <Grid container item xs={6} justifyContent="center">
+        <DropdownSelect isDisabled={false} />
+      </Grid>
+      <Grid container item xs={3} justifyContent="center">
+        <IconButton
+          size="large"
+          onClick={() => {
+            rightKeyPressed()
+          }}
+        >
+          <KeyboardArrowRight fontSize="large" />
+        </IconButton>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default MapPatchBrowser
