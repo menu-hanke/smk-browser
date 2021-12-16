7fc79e736ecd12ddf88526a334ca77e99b3aaf6c
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-12-16 14:10:42 +0200

    formatted tests. created new test for stand number feature

--- src/__tests__/createPolygonsFromXml.test.ts
+++ src/__tests__/createPolygonsFromXml.test.ts
@@ -4,48 +4,61 @@ import xml2js from 'xml2js'
 // @ts-ignore
 import wkt from 'wkt'
 import { createPolygonsFromXml } from '../renderer/controllers/createPolygonsFromXml'
+import GeoJSON from 'ol/format/GeoJSON'
+import turf from '@turf/turf'
 
 describe('createPolygonsFromXml', () => {
- it('should load multiple stands (both eterior-only and stands with single interiors)', async () => {
-  const xmlString = await readFile('src/__tests__/data1-stands.test.xml')
-  let xml = await xml2js.parseStringPromise(xmlString)
+  it('should load multiple stands (both eterior-only and stands with single interiors)', async () => {
+    const xmlString = await readFile('src/__tests__/data1-stands.test.xml')
+    let xml = await xml2js.parseStringPromise(xmlString)
 
-  expect(xml['ForestPropertyData']['st:Stands'][0]['st:Stand'].length).toBe(50)
+    expect(xml['ForestPropertyData']['st:Stands'][0]['st:Stand'].length).toBe(50)
 
-  const result = createPolygonsFromXml(xml)
+    const result = createPolygonsFromXml(xml)
 
-  expect(result).toBeTruthy()
-  expect(result.length).toBe(50)
+    expect(result).toBeTruthy()
+    expect(result.length).toBe(50)
 
-  expect(result[0].geometry.coordinates.length).toBe(1) // element 0 has only exterior
-  expect(result[15].geometry.coordinates.length).toBe(2) // element 15 has exterior and one interior
-  expect(result[21].geometry.coordinates.length).toBe(2) // element 21 has exterior and one interior
-  expect(result[29].geometry.coordinates.length).toBe(2) // element 29 has exterior and one interior
+    expect(result[0].geometry.coordinates.length).toBe(1) // element 0 has only exterior
+    expect(result[15].geometry.coordinates.length).toBe(2) // element 15 has exterior and one interior
+    expect(result[21].geometry.coordinates.length).toBe(2) // element 21 has exterior and one interior
+    expect(result[29].geometry.coordinates.length).toBe(2) // element 29 has exterior and one interior
 
-  expect(result[30].geometry.coordinates.length).toBe(1) // element 30 has only exterior
- })
+    expect(result[30].geometry.coordinates.length).toBe(1) // element 30 has only exterior
+  })
 
- it('should load single stand with multiple interiors', async () => {
-  const xmlString = await readFile('src/__tests__/data2-stands.test.xml')
-  let xml = await xml2js.parseStringPromise(xmlString)
+  it('should load single stand with multiple interiors', async () => {
+    const xmlString = await readFile('src/__tests__/data2-stands.test.xml')
+    let xml = await xml2js.parseStringPromise(xmlString)
 
-  expect(xml['ForestPropertyData']['st:Stands'][0]['st:Stand'].length).toBe(1)
+    expect(xml['ForestPropertyData']['st:Stands'][0]['st:Stand'].length).toBe(1)
 
-  const result = createPolygonsFromXml(xml)
+    const result = createPolygonsFromXml(xml)
 
-  expect(result).toBeTruthy()
-  expect(result.length).toBe(1)
+    expect(result).toBeTruthy()
+    expect(result.length).toBe(1)
 
-  expect(result[0].geometry.coordinates.length).toBe(4) // element 0 has exterior and 3 interiors
- })
+    expect(result[0].geometry.coordinates.length).toBe(4) // element 0 has exterior and 3 interiors
+  })
 
- it('should load empty stand XML)', async () => {
-  const xmlString = await readFile('src/__tests__/empty-stands.test.xml')
-  let xml = await xml2js.parseStringPromise(xmlString)
+  it('should load empty stand XML)', async () => {
+    const xmlString = await readFile('src/__tests__/empty-stands.test.xml')
+    let xml = await xml2js.parseStringPromise(xmlString)
 
-  const result = createPolygonsFromXml(xml)
+    const result = createPolygonsFromXml(xml)
 
-  expect(result).toBeTruthy()
-  expect(result.length).toBe(0)
- })
+    expect(result).toBeTruthy()
+    expect(result.length).toBe(0)
+  })
+
+  it('should return the stand number', async () => {
+    const xmlString = await readFile('src/__tests__/data2-stands.test.xml')
+    const xml = await xml2js.parseStringPromise(xmlString)
+    const polygonArray = createPolygonsFromXml(xml)
+
+    const features = new GeoJSON().readFeatures(turf.featureCollection(polygonArray))
+    const properties = features[0].getProperties()
+    const standNumber = properties.standNumber
+    expect(standNumber).toEqual(19)
+  })
 })
